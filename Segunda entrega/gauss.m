84059e02be83b7ae9d5690a013321ff252ac9465
iurregog <41337834+iurregog@users.noreply.github.com>
2019-11-03 11:58:15 -0500

    Add files via upload

--- Segunda entrega/gauss.m
+++ Segunda entrega/gauss.m
@@ -1,12 +1,14 @@
 function retval = gauss (vecini,nmax,tol,A,b)
 %%Gauss Seidel nu
+%%checked 
 format long
 D=diag(diag(A));
 L=-1* tril(A,-1);
 U=-1*triu(A,1);
-T=inv(D-L)*(U);
-C=inv(D-L)*b;
+T=inv(D-L)*(U)
+C=inv(D-L)*b
 
+radio=max(eig(T))
 i=0;
 error=tol+1;
 while i<nmax && error>tol
