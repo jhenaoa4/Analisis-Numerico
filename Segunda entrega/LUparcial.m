537f266b94cd7e58dcba064d888e2830f77d1c84
jhenaoa4 <41337763+jhenaoa4@users.noreply.github.com>
2019-11-04 18:19:35 -0500

    Add files via upload

---
+++ Segunda entrega/LUparcial.m
@@ -0,0 +1,45 @@
+function y=LUparcial(A,b,n)
+Ab= [A,b];
+L=eye(n);
+U=zeros(n);
+for k=1: (n)
+    Ab = pivoteoParcial(Ab,n,k);
+    for i=k+1: n
+       L(i,k) = Ab(i,k)/Ab(k,k);
+       for j= k :n+1
+           Ab(i,j)= Ab(i,j)- (L(i,k)*Ab(k,j));
+       end
+    end
+    U(1:k+1,1:n)=Ab(1:k+1,1:n);
+    Ab(1:n,1:n)
+    L
+    U
+end
+end
+
+function y=pivoteoParcial(Ab , n , k)
+numMayor = abs(Ab(k,k));
+filMayor = k;
+for s=k+1:n
+   if  abs(Ab(s,k)) > numMayor
+       numMayor = abs(Ab(s,k));
+       filMayor = s;
+   end
+end
+if numMayor == 0 
+    disp("EL SISTEMA NO TIENE SOLUCIÓN ÚNICA");
+else
+    if filMayor ~= k 
+        Ab=InterFil(Ab , filMayor , k);
+    end
+    y=Ab;
+end
+
+end
+
+function g=InterFil(A,f1 , f2)
+aux=A;
+aux(f1,:)=A(f2,:);
+aux(f2,:)=A(f1,:);
+g=aux;
+end
