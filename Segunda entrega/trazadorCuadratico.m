8984f04698873b952cb7acd689babafcf17cd4b1
Mauricio <41337819+mauricio2000@users.noreply.github.com>
2019-11-04 17:18:32 -0500

    Add files via upload

--- Segunda entrega/trazadorCuadratico.m
+++ Segunda entrega/trazadorCuadratico.m
@@ -1,34 +1,39 @@
 function trazadorCuadratico(x,y)
-N   =   length(x)-1;
-V   =   [0;zeros(2*N,1);zeros(N-1,1)];
-Z   =   zeros(length(V),length(V));
+N   = length(x)-1;
+V   = [0;zeros(2*N,1);zeros(N-1,1)];
+Z   = zeros(length(V),length(V));
 j=1;
 f=1;
 for i=2:2:2*N    
-    Z(i,f:f+2)          =   [x(j)^2 x(j) 1];
-    V(i)                =   y(j);
-    j                   =   j+1;
-    Z(i+1,f:f+2)        =   [x(j)^2 x(j) 1];  
-    V(i+1)              =   y(j);
-    f                   =   f+3;
+    Z(i,f:f+2) =   [x(j)^2 x(j) 1];
+    V(i) =   y(j);
+    j =   j+1;
+    Z(i+1,f:f+2) =   [x(j)^2 x(j) 1];  
+    V(i+1) =   y(j);
+    f =   f+3;
 end
 
 j=1;
 l=2;
 for i=2*N+2:3*N
     
-    Z(i,j:j+1)            =   [2*x(l) 1];
-    Z(i,j+3:j+4)          =   [-2*x(l) -1];
-    j                     =   j+3;
-    l                     =   l+1;
+    Z(i,j:j+1)=   [2*x(l) 1];
+    Z(i,j+3:j+4) =   [-2*x(l) -1];
+    j =   j+3;
+    l =   l+1;
 end
 
 Z(1,1)=1;
-Coeff       =       Z\V;
+Coeff = Z\V;
     j=1;
+    fprintf("X^2  X  1");
+    fprintf('\n', "");
     for n=1:length(Coeff)
-        a=Coeff(n,:);
-        fprintf('%f \t ', a);
+        a=Coeff(n,1);
+        if mod(n,3)==1 && n>1
+           fprintf('\n', ""); 
+        end
+        fprintf('%f \t', a);
     end
     for i=1:N
             hold on
@@ -37,4 +42,5 @@ Coeff       =       Z\V;
             j=j+3;
          plot(xi,yi,'b');
     end
+    fprintf('\n', "");
 end
