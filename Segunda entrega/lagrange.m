1489553821f2766687bbe091d7ab54cf38114308
Jose Miguel Gil Valencia <41337753+jmgilv@users.noreply.github.com>
2019-11-04 21:41:53 -0500

    Add files via upload

--- Segunda entrega/lagrange.m
+++ Segunda entrega/lagrange.m
@@ -1,16 +1,21 @@
 function [C,L] = lagrange(x,y)%x y son vectores de puntos ordenados
 n1=length(x);
 n=n1-1; %GRADO A LO SUMO n DEL POLINOMIO
-L=zeros(n1,n1);
+L=sym(n1);
+syms p;
 for k=1:n+1 %n+1 para leer todos los puntos del vector 
     v=1;
     for j=1:n+1
         if k~=j
-            v=conv(v,poly(x(j)))/(x(k)-x(j));
+            v=v*((p - x(j))/(x(k)- x(j)));
+            v=expand(v);
         end
+        j=j+1;
     end
-    L(k,:)=v;
+    L(k)=v;
+    L(k)
+    k=k+1;
 end
-C=y*L
+%C=y*L
 %L
 end
