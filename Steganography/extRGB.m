5387f36b584e35028545ffc2279a22a3e27b9ebb
iurregog <41337834+iurregog@users.noreply.github.com>
2019-11-09 15:30:19 -0500

    Update extRGB.m

--- Steganography/extRGB.m
+++ Steganography/extRGB.m
@@ -1,15 +1,20 @@
 function [message]=extRGB(img,auxX,auxY)
 [l,m,n]=size(img);
 [a,b]=size(auxX);
-message='';
+message=zeros([1 b]);
 
 for i=1:b
     str='';
     for k=1:3
-        str=strcat(str,extractBetween(img(auxX(i),auxY(i),k),7,9));
+        num=img(auxX(i),auxY(i),k);
+        nbin=dec2bin(num);
+        while(size(nbin)<9)
+        nbin= strcat('0',nbin);
+        end
+        str=strcat(str,extractBetween(nbin,7,9));
     end
     aux1=bin2dec(str);
-    aux2=char(aux1);
-    message=strcat(message,aux2);
+    message(i)=aux1;
+    message=char(message);
 end
 end
