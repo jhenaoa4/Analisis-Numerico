7139eff574aa5aecbbcc43d7e93b95ad1018a48c
iurregog <41337834+iurregog@users.noreply.github.com>
2019-11-09 15:39:23 -0500

    Update LSBinRGB.m

--- Steganography/LSBinRGB.m
+++ Steganography/LSBinRGB.m
@@ -6,25 +6,24 @@ function [img]=LSBinRGB(img,message,auxX,auxY)
 mASCII= dec2bin(double(message));
 [l,m,n]= size(img);
 [a,b]=size(message);
-p=1; %% position of the message you are saving
-%for i=1 : size(message)
+
     for j= 1:b
+        char= mASCII(j);
+        while(size(char)<9)
+        char= strcat('0',char);
+        end
+        splitChar=[extractBetween(char,1,3) extractBetween(char,4,6) extractBetween(char,7,9)];
+        
         for k= 1:n
           bin = dec2bin(img(auxX(j),auxY(j),k));
           while(size(bin)<9)
           bin= strcat('0',bin);
           end
           bin=  eraseBetween(bin,7,9);
-          char= mASCII(p);
-          while(size(char)<9)
-          char= strcat('0',char);
-          end
-          splitChar=[extractBetween(char,1,3) extractBetween(char,4,6) extractBetween(char,7,9)];
           bin= strcat(bin,splitChar(k));
           img(auxX(j),auxY(j),k)=bin2dec(bin);
-          p=p+1;
         end
     end
-    %%img
+
 imshow(img)
 end
